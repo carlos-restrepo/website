8b6eefcbaf224fade3402611d57c3ca22bb8f716
carlos-restrepo <carlos.restrepo@live.ca>
2023-07-05 12:55:43 -0400

    Changed port to function on heroku rnd 2

--- app.js
+++ app.js
@@ -351,7 +351,7 @@ app.post("/tutoring/request", function(req,res){
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
 
